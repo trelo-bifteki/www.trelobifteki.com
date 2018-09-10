f1d2b1653d61b05b210564e311548b91be82b0fa
Lampros Papadimitriou <papadimitriou@tyntec.com>
2018-09-10 22:31:51 +0200

    Using sass for NavigationBar component

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -32,86 +32,90 @@ export default {
   </nav>
 </template>
 
-<style scoped>
+<style lang="scss">
 
-ul {
-  display: flex;
-  flex-direction: row;
-}
+@import "../assets/variables.scss";
 
-li {
-  display: block;
-  min-width: 5rem;
-  text-align: center;
-  transition: background 0.2s ease, transform 0.33s ease, min-width 0.33s ease-in-out;
-  transform: skew(-20deg);  /* SKEW */
-}
+.navigation-bar {
+  ul {
+    display: flex;
+    flex-direction: row;
+  }
 
-a {
-  color: #3C3F50;
-  display:block;
-  text-decoration:none;
-  padding: 1rem;
-  font-size: 110%;
-  transform: skew(20deg); /* INVERSE SKEW */
-}
-
-img {
-  max-width: 100%;
-  max-height: 3rem;
-}
-
-li:first-child {
-  background-color: pink
-}
-
-li:first-child:hover {
-  background-color: pink;
-}
-
-li:nth-child(2) {
-  background-color:  #459E42;
-}
-
-li:nth-child(3) {
-  background-color: #F97C39;
-}
-
-li:nth-child(4) {
-  background-color:  #F7C931;
-}
-
-li:nth-child(5) {
-  background-color: #E52D98;
-}
-
-li:nth-child(6) {
-  background-color: #BA2766;
-}
-
-li:hover{
-  min-width: 7rem;
-}
-
-.navigation-bar__home {
-  padding: 0;
-}
-
-@media screen and (width < 768px) {
   li {
-    transform: skew(0deg);
+    display: block;
+    min-width: 5rem;
+    text-align: center;
+    transition: background 0.2s ease, transform 0.33s ease, min-width 0.33s ease-in-out;
+    transform: skew(-20deg);  /* SKEW */
   }
 
   a {
-    transform: skew(0deg);
+    display:block;
+    text-decoration:none;
+    padding: $space;
+    font-size: $font-size-ml;
+    transform: skew(20deg); /* INVERSE SKEW */
   }
 
   img {
-    transform: skewX(0deg);
+    max-width: 100%;
+    max-height: 3rem;
   }
 
-  nav {
-    overflow: hidden;
+  li:first-child {
+    background-color: pink
+  }
+
+  li:first-child:hover {
+    background-color: pink;
+  }
+
+  li:nth-child(2) {
+    background-color:  #459E42;
+  }
+
+  li:nth-child(3) {
+    background-color: #F97C39;
+  }
+
+  li:nth-child(4) {
+    background-color:  #F7C931;
+  }
+
+  li:nth-child(5) {
+    background-color: #E52D98;
+  }
+
+  li:nth-child(6) {
+    background-color: #BA2766;
+  }
+
+  li:hover{
+    min-width: 7rem;
+  }
+
+  .navigation-bar__home {
+    padding: 0;
+  }
+
+  @media screen and (max-width: 768px) {
+    li {
+      transform: skew(0deg);
+    }
+
+    a {
+      transform: skew(0deg);
+    }
+
+    img {
+      transform: skewX(0deg);
+    }
+
+    nav {
+      overflow: hidden;
+    }
   }
 }
+
 </style>
