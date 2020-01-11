2c534984cbbf7c0119b3a7b958e5f48e23cb1021
Lampros Papadimitriou <papadimitriou@tyntec.com>
2020-01-11 18:05:35 +0000

    Fix unit test

--- tests/unit/components/CookieNotification.spec.js
+++ tests/unit/components/CookieNotification.spec.js
@@ -31,10 +31,10 @@ describe('CookieNotification', () => {
     ).toBe(false);
   });
 
-  it('displays the cookie notification after is mounted', () => {
+  it('displays the cookie notification after is mounted', async () => {
     gdprService.isGdprAccepted.mockReturnValue(false);
     const wrapper = getWrapper();
-
+    await wrapper.vm.$nextTick();
     expect(
       wrapper.contains('.cookie-notification')
     ).toBe(true);
@@ -44,31 +44,38 @@ describe('CookieNotification', () => {
     gdprService.isGdprAccepted.mockReturnValue(false);
     const wrapper = getWrapper();
 
+    await wrapper.vm.$nextTick();
+
     expect(wrapper.vm.isVisible).toBe(true);
     expect(wrapper.vm.$ga).toBeDefined();
     const button = wrapper.find('.cookie-notification__button--ok');
+
     button.trigger('click');
+    await wrapper.vm.$nextTick();
 
     expect(
       $ga.enable
     ).toHaveBeenCalled();
-
     expect(
       wrapper.contains('.cookie-notification')
     ).toBe(false);
   });
 
-  it('closes the cookie notification after we click NO WAY button', () => {
+  it('closes the cookie notification after we click NO WAY button', async () => {
     gdprService.isGdprAccepted.mockReturnValue(false);
     const wrapper = getWrapper();
+    await wrapper.vm.$nextTick();
 
     expect(wrapper.vm.$ga).toBeDefined();
+    expect(wrapper.vm.isVisible).toBe(true);
     const button = wrapper.find('.cookie-notification__button--no');
     button.trigger('click');
 
     expect(
       $ga.disable
     ).toHaveBeenCalled();
+
+    await wrapper.vm.$nextTick();
 
     expect(
       wrapper.contains('.cookie-notification')
