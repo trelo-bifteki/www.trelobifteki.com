6cad572ec7b9f111966403a5413331968fb6dfc2
Lampros Papadimitriou <papadimitriou@tyntec.com>
2020-02-02 13:32:47 +0100

    Improve stylelint

--- src/components/CookieNotification.vue
+++ src/components/CookieNotification.vue
@@ -65,9 +65,9 @@ export default {
     background-color: #323232;
     border-radius: 5px;
     bottom: $space;
-    box-shadow: 0 3px 5px -1px rgba(0,0,0,.2),
-      0 6px 10px 0 rgba(0,0,0,.14),
-      0 1px 18px 0 rgba(0,0,0,.12);
+    box-shadow: 0 3px 5px -1px rgba(0, 0, 0, .2),
+      0 6px 10px 0 rgba(0, 0, 0, .14),
+      0 1px 18px 0 rgba(0, 0, 0, .12);
     color: $color-theme-white;
     font-size: 1rem;
     padding: $space $space-l;
