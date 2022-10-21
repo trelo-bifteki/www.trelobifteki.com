cfe32c03a981ba22687938c2253f3e233eaccef2
Lampros Papadimitriou <lambrospd@gmail.com>
2022-10-22 00:09:09 +0200

    Upgrade axios to latest version

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  transformIgnorePatterns: ["node_modules/(?!axios)"],
   reporters: [
     "default",
     "jest-junit"
